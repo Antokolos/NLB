d339ba2f0827a5508c63e75bd8b6cd6e7fcb5bd3
Antokolos <antokolos@gmail.com>
2014-04-09 01:24:41 +0700

    Windows version of build script was added

---
+++ build.bat
@@ -0,0 +1,10 @@
+call mvn package
+rd /S /Q dist
+mkdir dist
+copy NLBB\script\* dist
+mkdir dist\lib
+copy NLBB\target\NLBB-2.0-SNAPSHOT.jar dist\lib
+copy NLBB\target\dependency\* dist\lib
+xcopy NLBW\fonts dist\fonts /E /I /Y
+xcopy NLBW\xsl dist\xsl /E /I /Y
+call mvn clean
