6e81d8dc8305a2f3cfde3abc4cb88b80c84ecdc6
Anton P. Kolosov <antokolos@gmail.com>
2014-04-08 18:27:46 +0700

    Changed poms in order to use latest library versions in produced distro

--- build.sh
+++ build.sh
@@ -4,11 +4,7 @@ mkdir dist
 cp NLBB/script/* dist/
 mkdir dist/lib
 cp NLBB/target/NLBB-2.0-SNAPSHOT.jar dist/lib
-cp NLBL/target/NLBL-2.0-SNAPSHOT.jar dist/lib
-cp NLBW/target/NLBW-2.0-SNAPSHOT.jar dist/lib
-cp NLBB/lib/* dist/lib/
-cp NLBL/lib/* dist/lib/
-cp NLBW/lib/* dist/lib/
+cp NLBB/target/dependency/* dist/lib
 cp -r NLBW/fonts dist/fonts
 cp -r NLBW/xsl dist/xsl
 mvn clean
