e3d0604c8c35d2cf4663884726ccb0a70f6b5d29
Antokolos <antokolos@gmail.com>
2014-04-13 19:21:29 +0700

    run.bat was modified (32/64-bit definition code was added)

--- NLBB/script/run.bat
+++ NLBB/script/run.bat
@@ -1,6 +1,18 @@
-@ECHO OFF
-
-SET JAVA_HOME=C:\Java\jdk1.7.0_51
+@echo off
+ 
+Set RegQry=HKLM\Hardware\Description\System\CentralProcessor\0
+ 
+REG.exe Query %RegQry% > checkOS.txt
+ 
+Find /i "x86" < CheckOS.txt > StringCheck.txt
+ 
+If %ERRORLEVEL% == 0 (
+    Echo "This is 32 Bit Operating system"
+    SET JAVA_HOME=jre-7u51-windows-i586
+) ELSE (
+    Echo "This is 64 Bit Operating System"
+    SET JAVA_HOME=jre-7u51-windows-x64
+)
 
 :: Set application lib directory
 SET COMMON_LIB=lib\
