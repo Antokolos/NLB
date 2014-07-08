425c16f9c466db593dd2b105c72b106360be4166
Antokolos <antokolos@gmail.com>
2014-07-08 13:42:39 +0700

    Refactoring. NB: This is NEW candidate to format translator

--- NLBL/src/main/java/com/nlbhub/nlb/util/FileManipulator.java
+++ NLBL/src/main/java/com/nlbhub/nlb/util/FileManipulator.java
@@ -372,11 +372,12 @@ public class FileManipulator {
     ) throws NLBIOException, NLBFileManipulationException, NLBVCSException {
         ByteArrayInputStream inputStream = null;
         try {
-            if (mlsRootDir.exists() && !mlsRootDir.isDirectory()) {
+            boolean rootDirExists = mlsRootDir.exists();
+            if (rootDirExists && !mlsRootDir.isDirectory()) {
                 // TODO: Warning! Deleting old file when writing! This is done for backward compatibility and can be removed when all books will be converted.
                 deleteFileOrDir(mlsRootDir);
             }
-            if (!mlsRootDir.exists()) {
+            if (!rootDirExists) {
                 if (!mlsRootDir.mkdir()) {
                     throw new NLBFileManipulationException(
                             "Cannot create MultiLangString root: " + mlsRootDir.getCanonicalPath()
