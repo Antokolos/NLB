a560e457a39263b49d50e5d64d1a06fffd8e1db9
Antokolos <antokolos@gmail.com>
2014-09-15 20:02:53 +0700

    Placeholder image is now generated from the text on the fly

--- NLBB/src/main/java/com/nlbhub/nlb/builder/form/DialogMediaLibrary.java
+++ NLBB/src/main/java/com/nlbhub/nlb/builder/form/DialogMediaLibrary.java
@@ -1,9 +1,48 @@
+/**
+ * @(#)DialogMediaLibrary.java
+ *
+ * This file is part of the Non-Linear Book project.
+ * Copyright (c) 2012-2014 Anton P. Kolosov
+ * Authors: Anton P. Kolosov, et al.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License version 3
+ * as published by the Free Software Foundation with the addition of the
+ * following permission added to Section 15 as permitted in Section 7(a):
+ * FOR ANY PART OF THE COVERED WORK IN WHICH THE COPYRIGHT IS OWNED BY
+ * ANTON P. KOLOSOV. ANTON P. KOLOSOV DISCLAIMS THE WARRANTY OF NON INFRINGEMENT
+ * OF THIRD PARTY RIGHTS
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Affero General Public License for more details.
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program; if not, see http://www.gnu.org/licenses or write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA, 02110-1301 USA.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License.
+ *
+ * You can be released from the requirements of the license by purchasing
+ * a commercial license. Buying such a license is mandatory as soon as you
+ * develop commercial activities involving the Non-Linear Book software without
+ * disclosing the source code of your own applications.
+ *
+ * For more information, please contact Anton P. Kolosov at this
+ * address: antokolos@gmail.com
+ *
+ * Copyright (c) 2014 Anton P. Kolosov All rights reserved.
+ */
 package com.nlbhub.nlb.builder.form;
 
 import com.nlbhub.nlb.api.Constants;
 import com.nlbhub.nlb.api.MediaFile;
 import com.nlbhub.nlb.builder.model.ListSingleSelectionModel;
 import com.nlbhub.nlb.builder.model.MediaFileModelSwing;
+import com.nlbhub.nlb.builder.util.TextToGraphics;
 import com.nlbhub.nlb.domain.NonLinearBookFacade;
 import com.nlbhub.nlb.exception.NLBConsistencyException;
 import com.nlbhub.nlb.exception.NLBFileManipulationException;
@@ -118,7 +157,13 @@ public class DialogMediaLibrary extends JDialog {
                     if (StringHelper.notEmpty(placeholderName)) {
                         switch (mediaType) {
                             case Image:
-                                nonLinearBookFacade.addImageFile(PLACEHOLDER_IMAGE, placeholderName + PH_IMAGE_EXT);
+                                File tempFile = File.createTempFile("ph_img", "dat");
+                                tempFile.deleteOnExit();
+                                if (TextToGraphics.textToGraphics(placeholderName, tempFile)) {
+                                    nonLinearBookFacade.addImageFile(tempFile, placeholderName + PH_IMAGE_EXT);
+                                } else {
+                                    nonLinearBookFacade.addImageFile(PLACEHOLDER_IMAGE, placeholderName + PH_IMAGE_EXT);
+                                }
                                 break;
                             case Sound:
                                 nonLinearBookFacade.addSoundFile(PLACEHOLDER_SOUND, placeholderName + PH_SOUND_EXT);
@@ -126,7 +171,7 @@ public class DialogMediaLibrary extends JDialog {
                         }
                         m_mediaFileList.updateUI();
                     }
-                } catch (NLBConsistencyException | NLBIOException | NLBVCSException | NLBFileManipulationException ex) {
+                } catch (NLBConsistencyException | NLBIOException | NLBVCSException | NLBFileManipulationException | IOException ex) {
                     JOptionPane.showMessageDialog(
                             self,
                             "Error while adding: " + ex.toString()
---
+++ NLBB/src/main/java/com/nlbhub/nlb/builder/util/TextToGraphics.java
@@ -0,0 +1,89 @@
+/**
+ * @(#)TextToGraphics.java
+ *
+ * This file is part of the Non-Linear Book project.
+ * Copyright (c) 2012-2014 Anton P. Kolosov
+ * Authors: Anton P. Kolosov, et al.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License version 3
+ * as published by the Free Software Foundation with the addition of the
+ * following permission added to Section 15 as permitted in Section 7(a):
+ * FOR ANY PART OF THE COVERED WORK IN WHICH THE COPYRIGHT IS OWNED BY
+ * ANTON P. KOLOSOV. ANTON P. KOLOSOV DISCLAIMS THE WARRANTY OF NON INFRINGEMENT
+ * OF THIRD PARTY RIGHTS
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Affero General Public License for more details.
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program; if not, see http://www.gnu.org/licenses or write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA, 02110-1301 USA.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License.
+ *
+ * You can be released from the requirements of the license by purchasing
+ * a commercial license. Buying such a license is mandatory as soon as you
+ * develop commercial activities involving the Non-Linear Book software without
+ * disclosing the source code of your own applications.
+ *
+ * For more information, please contact Anton P. Kolosov at this
+ * address: antokolos@gmail.com
+ *
+ * Copyright (c) 2014 Anton P. Kolosov All rights reserved.
+ */
+package com.nlbhub.nlb.builder.util;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.FontMetrics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+
+public class TextToGraphics {
+    public static boolean textToGraphics(String text, File outputFile) {
+        try {
+            /*
+             * Because font metrics is based on a graphics context, we need to create
+             * a small, temporary image so we can ascertain the width and height
+             * of the final image
+             */
+            BufferedImage img = new BufferedImage(1, 1, BufferedImage.TYPE_INT_ARGB);
+            Graphics2D g2d = img.createGraphics();
+            Font font = new Font("Arial", Font.PLAIN, 48);
+            g2d.setFont(font);
+            FontMetrics fm = g2d.getFontMetrics();
+            int width = fm.stringWidth(text);
+            int height = fm.getHeight();
+            g2d.dispose();
+
+            img = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+            g2d = img.createGraphics();
+            g2d.setRenderingHint(RenderingHints.KEY_ALPHA_INTERPOLATION, RenderingHints.VALUE_ALPHA_INTERPOLATION_QUALITY);
+            g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+            g2d.setRenderingHint(RenderingHints.KEY_COLOR_RENDERING, RenderingHints.VALUE_COLOR_RENDER_QUALITY);
+            g2d.setRenderingHint(RenderingHints.KEY_DITHERING, RenderingHints.VALUE_DITHER_ENABLE);
+            g2d.setRenderingHint(RenderingHints.KEY_FRACTIONALMETRICS, RenderingHints.VALUE_FRACTIONALMETRICS_ON);
+            g2d.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+            g2d.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
+            g2d.setRenderingHint(RenderingHints.KEY_STROKE_CONTROL, RenderingHints.VALUE_STROKE_PURE);
+            g2d.setFont(font);
+            fm = g2d.getFontMetrics();
+            g2d.setColor(Color.BLACK);
+            g2d.drawString(text, 0, fm.getAscent());
+            g2d.dispose();
+            ImageIO.write(img, "png", outputFile);
+            return true;
+        } catch (IOException e) {
+            return false;
+        }
+    }
+}
