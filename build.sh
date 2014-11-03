68c41e64bf698ea8ec6f45b8790e705e924040bb
Antokolos <antokolos@gmail.com>
2014-11-03 18:45:11 +0400

    build script mod

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ rm -r dist
 mkdir dist
 cp NLBB/script/* dist/
 mkdir dist/lib
-cp NLBB/target/NLBB-2.0-SNAPSHOT.jar dist/lib
+cp NLBB/target/NLBB*.jar dist/lib
 cp NLBB/target/dependency/* dist/lib
 cp -r NLBW/fonts dist/fonts
 cp -r NLBW/xsl dist/xsl
