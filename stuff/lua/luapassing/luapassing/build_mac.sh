be2492a9339242f4bcca73e35f81515dba093691
Antokolos <antokolos@gmail.com>
2015-08-30 01:54:02 +0600

    Redundant files were removed

--- stuff/lua/luapassing/luapassing/build_mac.sh
+++ stuff/lua/luapassing/luapassing/build_mac.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+# g++ -arch i386 -I/path/to/steam/includes -D_MACOSX -o luapassing.so -shared luapassing.cpp adapter.cpp libsteam_api.dylib -ldl -fPIC
 g++ -arch i386 -D_MACOSX -o luapassing.so -shared luapassing.cpp adapter.cpp -ldl -fPIC
