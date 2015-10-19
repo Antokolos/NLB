2553d17e1b9bd955aee6252a1a420be89239e0a7
Antokolos <antokolos@gmail.com>
2015-10-19 17:46:26 +0600

    Added steam lib so to build script

--- stuff/lua/luapassing/luapassing/build_linux.sh
+++ stuff/lua/luapassing/luapassing/build_linux.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-g++ -I/home/apkolosov/work/Steam/sdk/public/steam -D_LINUX -o luapassing.so -shared luapassing.cpp adapter.cpp -fPIC
+g++ -I/home/apkolosov/work/Steam/sdk/public/steam -D_LINUX -o luapassing.so -shared luapassing.cpp adapter.cpp libsteam_api.so -fPIC
