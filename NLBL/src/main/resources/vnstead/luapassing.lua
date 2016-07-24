ad57d731c957828ec06b6948a2d264fa16e086cd
Antokolos <antokolos@gmail.com>
2016-07-24 20:59:44 +0600

    dev::NLB-167::luapassing.lua was added

---
+++ NLBL/src/main/resources/vnstead/luapassing.lua
@@ -0,0 +1,60 @@
+local initFunc = function()
+end
+
+local setAchievementFunc = function(achievementName)
+end
+
+local storeFunc = function()
+end
+
+local clearAchievementFunc = function(achievementName)
+end
+
+function init()
+    _statsInitDone = false;
+end
+
+statsAPI = {
+    init = function()
+        print("Initializing API...\n");
+        if (_statsInitDone) then
+            print("Already initialized!\n");
+            return 0.0;
+        else
+            initFunc();
+            _statsInitDone = true;
+            print("API initialized.\n");
+            return 1.0;
+        end
+    end,
+    setAchievement = function(achievementName, storeImmediately)
+        print("Setting achievement '"..achievementName.."'...\n");
+        setAchievementFunc(achievementName);
+        if (storeImmediately) then
+            storeFunc();
+        end
+        print("Achievement set.\n");
+        return 0.0;
+    end,
+    store = function()
+        print("Storing...\n");
+        storeFunc();
+        print("Done.\n");
+        return 0.0;
+    end,
+    clearAchievement = function(achievementName, storeImmediately)
+        print("Clearing achievement '"..achievementName.."'...\n");
+        clearAchievementFunc(achievementName);
+        if (storeImmediately) then
+            storeFunc();
+        end
+        print("Achievement cleared.\n");
+        return 0.0;
+    end,
+    resetAll = function()
+        print("Resetting all achievements...\n");
+        resetAllFunc();
+        print("Done.\n");
+        return 0.0;
+    end
+}
