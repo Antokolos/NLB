bef90fe4d3a153f885ebc290ef6d01f204f9df37
Antokolos <antokolos@gmail.com>
2017-10-17 13:25:17 +0600

    dev::NLB-249::Rework standard (non-VN) theme

--- NLBW/res/vnstead/themes/default/theme_standard.lua
+++ NLBW/res/vnstead/themes/default/theme_standard.lua
@@ -3,6 +3,8 @@ local tr = nlb:theme_root();
 theme.win.geom(68, 670, 1704, 301);
 theme.inv.geom(68, 982, 1704, 90);
 
+theme.set('scr.gfx.bg', tr .. 'gfx/bg.jpg');
+
 -- http://www.fonts2u.com/steinem-unicode.font
 theme.set('win.fnt.name', tr .. 'fonts/{sans,sans-b,sans-i,sans-bi}.ttf');
 theme.set('inv.fnt.name', tr .. 'fonts/{sans,sans-b,sans-i,sans-bi}.ttf');
