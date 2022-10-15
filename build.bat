2bc9ad4751fd79d7d64bf55b51e81cad481ee80b
Antokolos <antokolos@gmail.com>
2022-10-15 22:57:58 +0600

    Autoincrement versions plugin fix

--- build.bat
+++ build.bat
@@ -3,7 +3,7 @@ rd /S /Q dist
 mkdir dist
 copy NLBB\script\* dist
 mkdir dist\lib
-copy NLBB\target\NLBB-2.0-SNAPSHOT.jar dist\lib
+copy NLBB\target\NLBB*.jar dist\lib
 copy NLBB\target\dependency\* dist\lib
 xcopy NLBW\fonts dist\fonts /E /I /Y
 xcopy NLBW\xsl dist\xsl /E /I /Y
