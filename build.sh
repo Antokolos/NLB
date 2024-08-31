31d50bd14295dc4a28956950c05c76ee3db085e0
Antokolos <antokolos@gmail.com>
2024-08-31 21:34:58 +0600

    Project version was autoincremented

--- build.sh
+++ build.sh
@@ -1,10 +1,11 @@
-mvn package
+mvn generate-sources package
 rm -r dist
 mkdir dist
 cp NLBB/script/* dist/
 mkdir dist/lib
 cp NLBB/target/NLBB*.jar dist/lib
-cp NLBB/target/dependency/* dist/lib
+cp NLBB/target/lib/* dist/lib
+cp NLBB/target/modules/* dist/lib
 cp -r NLBW/fonts dist/fonts
 cp -r NLBW/xsl dist/xsl
 cp -r NLBW/cfg dist/cfg
